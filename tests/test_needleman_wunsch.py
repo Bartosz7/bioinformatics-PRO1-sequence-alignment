a493af3034ca6f1d5e4dee8176a147dea57efc47
Bartosz7 <bartoszgrabek7@gmail.com>
2024-10-28 08:15:25 +0100

    add: add NW TCs for long sequences with n limiter

--- tests/test_needleman_wunsch.py
+++ tests/test_needleman_wunsch.py
@@ -3,6 +3,7 @@ Testcases for the Needleman-Wunsch algorithm implementation.
 
 Most results tests are based on the outputs of another software:
 https://bioboot.github.io/bimm143_W20/class-material/nw/
+http://rna.informatik.uni-freiburg.de/Teaching/index.jsp?toolName=Smith-Waterman
 
 @author: Bartosz Grabek
 @date: 27-10-2024
@@ -120,23 +121,53 @@ class TestNeedlemanWunsch(unittest.TestCase):
     def test_results_3_multiple_paths(self):
         nw = NeedlemanWunsch(submatrix_file="data/submatrix4.csv", GP=-2)
         all_paths, score = nw.compare("GATTACA", "GTCGACGCA", n=10)
-        expected_score_matrix = np.array([
-            [0, -2, -4, -6, -8, -10, -12, -14],
-            [-2, 1, -1, -3, -5, -7, -9, -11],
-            [-4, -1, 0, 0, -2, -4, -6, -8],
-            [-6, -3, -2, -1, -1, -3, -3, -5],
-            [-8, -5, -4, -3, -2, -2, -4, -4],
-            [-10, -7, -4, -5, -4, -1, -3, -3],
-            [-12, -9, -6, -5, -6, -3, 0, -2],
-            [-14, -11, -8, -7, -6, -5, -2, -1],
-            [-16, -13, -10, -9, -8, -7, -4, -3],
-            [-18, -15, -12, -11, -10, -7, -6, -3]
-        ])
-        expected_dir_matrix = [['0', 'L', 'L', 'L', 'L', 'L', 'L', 'L'], ['U', 'D', 'L', 'L', 'L', 'L', 'L', 'L'], ['U', 'U', 'D', 'D', 'DL', 'L', 'L', 'L'], ['U', 'U', 'DU', 'D', 'D', 'DL', 'D', 'L'], ['U', 'DU', 'DU', 'DU', 'D', 'D', 'DL', 'D'], ['U', 'U', 'D', 'DU', 'DU', 'D', 'DL', 'D'], ['U', 'U', 'U', 'D', 'DU', 'U', 'D', 'L'], ['U', 'DU', 'U', 'DU', 'D', 'U', 'U', 'D'], ['U', 'U', 'U', 'DU', 'DU', 'DU', 'DU', 'DU'], ['U', 'U', 'DU', 'DU', 'DU', 'D', 'U', 'D']]
+        expected_score_matrix = np.array(
+            [
+                [0, -2, -4, -6, -8, -10, -12, -14],
+                [-2, 1, -1, -3, -5, -7, -9, -11],
+                [-4, -1, 0, 0, -2, -4, -6, -8],
+                [-6, -3, -2, -1, -1, -3, -3, -5],
+                [-8, -5, -4, -3, -2, -2, -4, -4],
+                [-10, -7, -4, -5, -4, -1, -3, -3],
+                [-12, -9, -6, -5, -6, -3, 0, -2],
+                [-14, -11, -8, -7, -6, -5, -2, -1],
+                [-16, -13, -10, -9, -8, -7, -4, -3],
+                [-18, -15, -12, -11, -10, -7, -6, -3],
+            ]
+        )
+        expected_dir_matrix = [
+            ["0", "L", "L", "L", "L", "L", "L", "L"],
+            ["U", "D", "L", "L", "L", "L", "L", "L"],
+            ["U", "U", "D", "D", "DL", "L", "L", "L"],
+            ["U", "U", "DU", "D", "D", "DL", "D", "L"],
+            ["U", "DU", "DU", "DU", "D", "D", "DL", "D"],
+            ["U", "U", "D", "DU", "DU", "D", "DL", "D"],
+            ["U", "U", "U", "D", "DU", "U", "D", "L"],
+            ["U", "DU", "U", "DU", "D", "U", "U", "D"],
+            ["U", "U", "U", "DU", "DU", "DU", "DU", "DU"],
+            ["U", "U", "DU", "DU", "DU", "D", "U", "D"],
+        ]
         self.assertTrue(np.array_equal(nw.score_matrix, expected_score_matrix))
         self.assertEqual(nw.dir_matrix, expected_dir_matrix)
         self.assertEqual(score, -3)
-        self.assertEqual(set(all_paths), set([('GATTAC--A', 'GTCGACGCA'), ('GATTA--CA', 'GTCGACGCA')]))
+        self.assertEqual(
+            set(all_paths),
+            set([("GATTAC--A", "GTCGACGCA"), ("GATTA--CA", "GTCGACGCA")]),
+        )
+
+    def test_results_4_multiple_paths_long_sequences(self):
+        """This test case yields 24 optimal paths. We test the n=10 limit."""
+        n = 10
+        nw = NeedlemanWunsch(submatrix_file="data/submatrix2.csv", GP=-2)
+        all_paths, score = nw.compare("AGTAGTTTCGGATGATAACA", "ATCGAGGCAGTGTATGATTA", n=n)
+        self.assertEqual(score, 41)
+        self.assertEqual(len(all_paths), n)
+        self.assertTrue(
+            set(all_paths).issubset(
+                set([('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG--GCAGTGTATGAT--TA'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG--GCAGTGTATGAT--TA'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG-G-CAGTGTATGAT--TA'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG-G-CAGTGTATGAT--TA'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAGG--CAGTGTATGAT--TA'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AGG--CAGTGTATGAT--TA'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG--GCAGTGTATGAT-T-A'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG--GCAGTGTATGAT-T-A'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG-G-CAGTGTATGAT-T-A'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG-G-CAGTGTATGAT-T-A'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAGG--CAGTGTATGAT-T-A'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AGG--CAGTGTATGAT-T-A'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG--GCAGTGTATGATT--A'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG--GCAGTGTATGATT--A'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG-G-CAGTGTATGATT--A'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG-G-CAGTGTATGATT--A'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAGG--CAGTGTATGATT--A'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AGG--CAGTGTATGATT--A'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG--GCAGTGTATGATTA--'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG--GCAGTGTATGATTA--'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAG-G-CAGTGTATGATTA--'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AG-G-CAGTGTATGATTA--'), ('AGT--AGTTTC-G-G-ATGATAACA', 'A-TCGAGG--CAGTGTATGATTA--'), ('A--GTAGTTTC-G-G-ATGATAACA', 'ATCG-AGG--CAGTGTATGATTA--')])
+            )
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
